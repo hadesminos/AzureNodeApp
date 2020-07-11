db9ffbc27ab41d0e987bcdd289e284285ee7a09a
hadesminos <hades_minos@hotmail.com>
2020-07-11 15:46:04 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var ua = require('universal-analytics');
-  var visitor = ua('UA-165884413-8');
+  var uuid = require('uuid');
+
+  var visitor = ua('UA-165884413-8', req.connection.remoteAddress);
   visitor.pageview("/test").send();
-  res.render('index', { title: 'Express' });
+  visitor.event("Event Category", "Event Action").send();
+  
+  res.render('index', { title: 'Express ' + req.connection.remoteAddress });
 });
 
 module.exports = router;
