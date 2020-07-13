8210f4687a153ccb6b5310e37e9a529cdb98d0b8
unknown <hades_minos@hotmail.com>
2020-07-13 11:09:03 -0500

    add

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const ua = require('universal-analytics');
+const uuid = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var ua = require('universal-analytics');
-  var uuid = require('uuid');
 
-  //var visitor = ua('UA-165884413-8', req.connection.remoteAddress);
-  //visitor.pageview("/test").send();
-  //visitor.event("Event Category", "Event Action").send();
+
+  var visitor = ua('UA-165884413-8', req.connection.remoteAddress);
+  visitor.pageview("/test").send();
+  visitor.event("Event Category", "Event Action").send();
   
-
-  res.render('index', { title: 'Express ' + req.ip });
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.render('index', { title: 'Express ' + req.ip});
 });
 
 module.exports = router;
