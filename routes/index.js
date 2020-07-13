7e0d4e22d44b183ccd5860f61e7da9b8b0f9a04a
unknown <hades_minos@hotmail.com>
2020-07-13 13:31:37 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ const ua = require('universal-analytics');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const ip =  req.ip.split(":").shift();
 
-
-  var visitor = ua('UA-165884413-8', cid = req.ip.split(":").shift());
+  var visitor = ua('UA-165884413-8', cid = ip);
   visitor.pageview("/test").send();
   visitor.event("Event Category", "Event Action").send();
   
   //const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.render('index', { title: 'Express ' + req.ip});
+  res.render('index', { title: 'Express ' + ip});
 });
 
 module.exports = router;
