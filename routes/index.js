d8790083a6c90bee392957bf338ae58c9edd10cc
unknown <hades_minos@hotmail.com>
2020-07-13 15:32:09 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,16 @@ router.get('/', function(req, res, next) {
   //const { GA, ClientDIP } = req.query;
 
   //var visitor = ua( req.query.GA, cid = req.query.cid);
-  var visitor = ua( UA-165884413-8, cid = '186.87.8.104');
-  visitor.pageview("/" + req.query.Page).send();
-  visitor.event("Event Category", "Event Action").send();
+  var visitor = ua( 'UA-165884413-8', cid = req.ip.split(':').shift());
+  //visitor.pageview("/" + req.query.Page).send();
+  visitor.pageview("/").send();
+  //visitor.event("Event Category", "Event Action").send();
   
   //const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   //res.render('index', { title: 'Express ' + ip});
   //res.send('ga ' + req.query.GA + ' client ' + req.query.cid);
-  res.send('Success');
+  res.send(' client ' + req.ip.split(':').shift());
+  //res.send('Success');
 });
 
 module.exports = router;
