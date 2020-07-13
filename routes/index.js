17eaff476a8b0390479bfa4e3cfcebcc0e3b51ac
unknown <hades_minos@hotmail.com>
2020-07-13 15:51:10 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,35 @@ const ua = require('universal-analytics');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //const  GA = req.query.GA;
+  var GA = req.query.GA;
   //const cid = req.query.cid;
   //const { GA, ClientDIP } = req.query;
   const cid = req.ip.split(':').shift();
 
-  //var visitor = ua( req.query.GA, cid = req.query.cid);
-  var visitor = ua( tid = 'UA-165884413-8',{
+  var visitor = ua( tid = GA, {
     cid: cid,
     uid: cid,
     strictCidFormat: false,
   });
-  //visitor.pageview("/" + req.query.Page).send();
-  visitor.pageview("/").send();
+
+  visitor.pageview("/" + req.query.Page).send();
+
+  res.send('Success');
+   
+  //var visitor = ua( tid = 'UA-165884413-8',{
+  //  cid: cid,
+  //  uid: cid,
+  //  strictCidFormat: false,
+ // });
+
+  //visitor.pageview("/").send();
   //visitor.event("Event Category", "Event Action").send();
   
   //const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   //res.render('index', { title: 'Express ' + ip});
   //res.send('ga ' + req.query.GA + ' client ' + req.query.cid);
-  res.send(' client id ' + cid);
-  //res.send('Success');
+  //res.send(' client id ' + cid);
+ 
 });
 
 module.exports = router;
