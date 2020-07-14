8c586fcf7b3afd30a2c174095d00a740a2af7d1c
unknown <hades_minos@hotmail.com>
2020-07-14 10:28:41 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -13,36 +13,14 @@ router.get('/', function(req, res, next) {
   const uip = req.ip.split(':').shift();
   var visitor = ua( tid = GA, {
     cid: uid,
-    //uid: uid,
+    uid: uid,
     strictCidFormat: false
   });
 
   visitor.set('uip', uip);
   visitor.pageview("/" + req.query.Page).send();
 
-  /*
-  const analyticsMiddleware = (req, res, next) => {
-    const userId = req && req.user && req.user._id 
-    req.visitor.set("uid", userId);
-    req.visitor.pageview(req.path).send();
-    next();
-}
-*/
-  //res.send('Success');
-   
-  //var visitor = ua( tid = 'UA-165884413-8',{
-  //  cid: cid,
-  //  uid: cid,
-  //  strictCidFormat: false,
- // });
 
-  //visitor.pageview("/").send();
-  //visitor.event("Event Category", "Event Action").send();
-  
-  //const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  //res.render('index', { title: 'Express ' + ip});
-  //res.send('ga ' + req.query.GA + ' client ' + req.query.cid);
-  //res.send(' client id ' + uuid.v4());
   res.send('Success with uid ' + uid);
  
 });
