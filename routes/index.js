52f3e51c478df2ad90ec9ad5e58f0cc77cb2d6f2
unknown <hades_minos@hotmail.com>
2020-07-14 09:18:17 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.get('/', function(req, res, next) {
     cid: cid,
     uid: cid,
     strictCidFormat: false,
-    uip: '40.122.65.162', //uip,
-    aip=0
+    uip: '40.122.65.162' //uip,
+   
   });
 
   //visitor.set('40.122.65.162');
