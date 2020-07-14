0a6d92679f3aafbcd573371adada4fc5630befb0
unknown <hades_minos@hotmail.com>
2020-07-14 09:16:11 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
     uip: '40.122.65.162' //uip
   });
 
-  visitor.set(uip= '40.122.65.162');
+  //visitor.set('40.122.65.162');
   visitor.pageview("/" + req.query.Page).send();
 
   /*
