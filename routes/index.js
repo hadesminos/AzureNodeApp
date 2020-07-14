96cf6c165acf85eeb3ce2547aa55a6a95032e2ea
unknown <hades_minos@hotmail.com>
2020-07-14 09:36:58 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,14 @@ router.get('/', function(req, res, next) {
   var GA = req.query.GA;
   //const cid = req.query.cid;
   //const { GA, ClientDIP } = req.query;
-  const cid = req.ip.split(':').shift();
   const uip = req.ip.split(':').shift();
-  const userId = req && req.user && req.user._id ;
-
   var visitor = ua( tid = GA, {
-    cid: cid,
-    uid: cid,
+    cid: uid,
+    uid: uid,
     strictCidFormat: false
   });
 
-  visitor.set('uip','181.78.5.6');
+  visitor.set('uip',uip);
   visitor.pageview("/" + req.query.Page).send();
 
   /*
